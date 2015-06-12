c43edf11af421603d92ffaf8818e59012879bc21
yus <yus.projectproto@gmail.com>
2015-06-12 23:06:16 +0800

    added BluetoothRfcommClient class

--- app/src/main/java/com/ymsoftlabs/btjoystick/BluetoothJoystickActivity.java
+++ app/src/main/java/com/ymsoftlabs/btjoystick/BluetoothJoystickActivity.java
@@ -8,6 +8,17 @@ import android.view.MenuItem;
 
 public class BluetoothJoystickActivity extends ActionBarActivity {
 
+    // Message types sent from the BluetoothRfcommClient Handler
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+
+    // Key names received from the BluetoothRfcommClient Handler
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
